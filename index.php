1a33f8a8f3a908f68af502a33555005c1ff1490b
ezleszaz <59374702+ezleszaz@users.noreply.github.com>
2019-12-31 00:07:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,5 @@
   <h1> Ny.S POLITICAL </h1>
   <h2> Üdvözöllek idetévedt internetező! Ez egy politikai blog ahol egyszerű állampolgárok véleménye jelenik meg. Ny.S. </h3
   <h3> Aktuális cikkek <h3> 
-</body>
-</HTML>
+ </body>
+ </HTML>
