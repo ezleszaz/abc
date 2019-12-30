b656dac7a4bb203808ff6917db6acbdfb59f6859
ezleszaz <59374702+ezleszaz@users.noreply.github.com>
2019-12-31 00:07:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
  body bgcolor="#8FBC8F"
   <h1> Ny.S POLITICAL </h1>
-  <h2> Üdvözöllek idetévedt internetező! Ez egy politikai blog ahol egyszerű állampolgárok véleménye jelenik meg. Ny.S. </h3
-  <h3> Aktuális cikkek <h3> 
+  <h2> Üdvözöllek idetévedt internetező! Ez egy politikai blog ahol egyszerű állampolgárok véleménye jelenik meg. Ny.S. </3>
+  <h3> Aktuális cikkek </h3> 
  </body>
  </HTML>
