b343da0d8f6765498e6d0b696ad17e789a24a099
ezleszaz <59374702+ezleszaz@users.noreply.github.com>
2019-12-31 01:18:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,104 @@
-<HTML>
+<html>
 <head>
-  <tittle><b> Hello there, szerkesztés alatt </b></tittle>
-  </head>
+<meta http-equiv="Content-Language" content="hu" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hello there</title>
+<style type="text/css">
+.style1 {
+	text-align: center;
+	color: rgb(128, 0, 128);
+	font-size: xx-large;
+}
+.style2 {
+	font-family: "Malgun Gothic";
+	font-weight: normal;
+	color: rgb(128, 0, 0);
+}
+.style3 {
+	font-family: Arial, Helvetica, sans-serif;
+}
+.style4 {
+	color: #800080;
+	font-weight: bold;
+}
+.style5 {
+	font-family: Arial, Helvetica, sans-serif;
+	color: rgb(128, 0, 0);
+}
+</style>
+</head>
+
 <body>
- body bgcolor="#8FBC8F"
-  <h1> Ny.S POLITICAL </h1>
-  <h2> Üdvözöllek idetévedt internetező! Ez egy politikai blog ahol egyszerű állampolgárok véleménye jelenik meg. Ny.S. </h2>
-  <h3> Aktuális cikkek </h3> 
- </body>
- </HTML>
+
+<tittle style="color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;"></tittle>
+<body bgcolor="#228B22">
+<span class="style4">Hello there&nbsp;... Az oldal folyamatos szerkesztés és frissítés alatt 
+...&nbsp; </span>
+<h1 style="font-family: &quot;Times New Roman&quot;; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;" class="style1">
+Polgar Political</h1>
+<h2 style="font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;" class="style5">
+Üdvözöllek idetévedt internetező!<br />
+ Ez egy politikai blog ahol egyszerű <br />
+állampolgárok véleménye jelenik meg.<br />
+ Ny.S.</h2>
+<h3 style="font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;" class="style2">
+&nbsp;</h3>
+<h3 style="font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;" class="style2">
+&nbsp;</h3>
+<h3 style="font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;" class="style2">
+<strong>Aktuális cikkek:</strong></h3>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>
+<img alt="foszerkeszto" src="img5.jpg" height="117" /> 
+Az oldal főszerkesztője ; Ny.S.</p>
+<p>&nbsp;</p>
+</body>
+</html>
+
